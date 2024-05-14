85694759468b60bd6c446c01c59389175c26348f
iysaofficial <youngscientist.iysa@gmail.com>
2024-05-14 10:33:40 +0700

    fix typo

--- src/components/home/HeroComp.jsx
+++ src/components/home/HeroComp.jsx
@@ -5,8 +5,8 @@ const HeroComp = () =>{
             <div className="hero-container">
                 <img src="./assets/images/logo/NYBO.png" alt="" />
                 <div className="hero-text">
-                    <h1>National Youth Bioloy Olympiad</h1>
-                    <p>Pendaftaran National Youth Bioloy Olympiad telah dibuka! Bergabunglah dengan acara bergengsi ini dan dapatkan pengalaman yang tak terlupakan!</p>
+                    <h1>National Youth Biology Olympiad</h1>
+                    <p>Pendaftaran National Youth Biology Olympiad telah dibuka! Bergabunglah dengan acara bergengsi ini dan dapatkan pengalaman yang tak terlupakan!</p>
                     <br />
                     <a href="#">Segera Hadir 2025</a>
                 </div>
